83b2c1e0ef9123bd8b8c8eb02789ad425dca7fcc
jesuschejo <chejojesus@gmail.com>
2020-08-31 21:15:41 -0300

    cambio

--- app/Http/Controllers/ControladorTransferenciabancaria.php
+++ app/Http/Controllers/ControladorTransferenciabancaria.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Entidades\Medio_pago;
-use App\Entidades\Sistema\Menu;
+
  
 require app_path().'/start/constants.php';
 use Session;
  
 class ControladorTransferenciabancaria extends Controller{
+
    public function nuevo(){
+
        $titulo = "Nueva tranferencia bancaria";
        return view('transferenciabancaria.tranferenciabancaria', compact('titulo'));   
    }
