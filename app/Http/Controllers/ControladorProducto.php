f1d24451132315099235cbc09c7296ba96d14af3
roisbert <roisbertalmenara@gmail.com>
2020-08-31 21:46:08 -0300

    ultimo_cambio_roisbert

--- app/Http/Controllers/ControladorProducto.php
+++ app/Http/Controllers/ControladorProducto.php
@@ -4,14 +4,19 @@ namespace App\Http\Controllers;
  
 use Illuminate\Http\Request;
 use App\Entidades\Sistema\Productos;
+use App\Entidades\Sucursal;
+
+
 require app_path().'/start/constants.php';
 use Session;
  
 class ControladorProducto extends Controller{
    public function nuevo(){
        $titulo = "Nuevo producto";
- 
-      return view('producto.producto-nuevo', compact('titulo'));   
+    $sucural = new Sucursal();
+    $array_sucursal = $sucural->obtenerTodos();
+      return view('producto.producto-nuevo', compact('titulo', 'array_sucursal'));   
    }
  
 }
+
