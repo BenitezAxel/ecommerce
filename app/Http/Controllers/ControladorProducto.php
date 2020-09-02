e1d6faa5a86a9c174f0578da99142f88eabe4bcd
ntarche <nelson.tarche@gmail.com>
2020-09-02 18:58:13 -0300

    cambios

--- app/Http/Controllers/ControladorProducto.php
+++ app/Http/Controllers/ControladorProducto.php
@@ -1,22 +1,19 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
-use Illuminate\Http\Request;
-use App\Entidades\Sistema\Productos;
+
 use App\Entidades\Sucursal;
 
+require app_path() . '/start/constants.php';
 
-require app_path().'/start/constants.php';
-use Session;
- 
-class ControladorProducto extends Controller{
-   public function nuevo(){
-       $titulo = "Nuevo producto";
-    $sucural = new Sucursal();
-    $array_sucursal = $sucural->obtenerTodos();
-      return view('producto.producto-nuevo', compact('titulo', 'array_sucursal'));   
-   }
- 
+class ControladorProducto extends Controller
+{
+    public function nuevo()
+    {
+        $titulo = "Nuevo producto";
+        $sucursal = new Sucursal();
+        $array_sucursal = $sucursal->obtenerTodos();
+        return view('producto.producto-nuevo', compact('titulo', 'array_sucursal'));
+    }
+
 }
-
