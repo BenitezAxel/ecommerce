1fd89b30f6f16eb078b7e9749afd43d9576599d9
BenitezAxel <benitezaxel03@gmail.com>
2020-09-07 20:12:54 -0300

    cambios mercadopago

--- app/Http/Controllers/ControladorMercadopago.php
+++ app/Http/Controllers/ControladorMercadopago.php
@@ -13,8 +13,25 @@ class ControladorMercadopago extends Controller{
 
    public function nuevo(){
 
-       $titulo = "Mercado Pago";
+       $titulo = "Metodo Mercado Pago";
+       $mediodepago = new Medio_pago();
+        $array_mediodepago = $mediodepago->obtenerTodos();   
        return view('mercadopago.mercadopago', compact('titulo'));   
+   }
+   public function guardar($request){
+       $titulo = "Mercado Pago";
+       $entidadMediopago = new Medio_pago();
+       $entidadMediopago->cargarDesdeRequest($request);
+
+       if ($entidadMediopago->idmediopago){
+           $msg["ESTADO"] = MSG_ERROR;
+       }else{
+        if($_POST["id"] > 0){
+            $entidadMediopago->guardar();
+        }else{
+            $entidadMediopago->insertar();
+        }
+       }
    }
 
 }
